5318af783618f0bd11dccd1db331267442a39b7d
Rishabh Yadav <1998rishabh31@gmail.com>
2022-02-26 20:49:26 +0530

    Create Data Hiding.java

---
+++ Data Hiding.java
@@ -0,0 +1,57 @@
+import java.lang.*;
+import java.util.*;
+
+class Rectangle
+{
+    private int length;
+    private int breadth;
+    
+    public void setLength(int x)
+    {
+        if(x>0)
+            length = x;
+        else 
+            length = 1;
+    }
+    
+    public void setBreadth(int x)
+    {
+        if(x>0)
+            breadth = x;
+        else 
+            breadth = 1;
+    }
+    
+    public int getLength()
+    {
+        return length;
+    }
+    
+    public int getBreadth()
+    {
+        return breadth;
+    }
+    
+    public int area()
+    {
+        return length*breadth;
+    }
+    
+    public int perimeter()
+    {
+        return 2*(length+breadth);
+    }
+}
+
+class Main
+{
+    public static void main(String args[])
+    {
+        Rectangle r1 = new Rectangle();
+        r1.setLength(5);
+        r1.setBreadth(3);
+        
+        System.out.println("Area of Rectangle is "+r1.area());
+        System.out.println("Perimeter of Rectangle is "+r1.perimeter());
+    }
+}
