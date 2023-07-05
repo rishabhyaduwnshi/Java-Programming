831937dff6ed6ffe34ae50d569cf017643abb6e4
Rishabh Yadav <1998rishabh31@gmail.com>
2023-07-05 15:13:01 +0530

    Update Writing to a File.java

--- File Handling/Writing to a File.java
+++ File Handling/Writing to a File.java
@@ -9,8 +9,22 @@ class Main
         try
         {
             FileOutputStream fos = new FileOutputStream("TextFile.txt");
-            String str = "Learning Java Programming";
-            fos.write(str.getBytes());
+            
+            //1st Method
+            String str1 = "Learning Java Programming\n";
+            fos.write(str1.getBytes());
+            
+            //2nd Method - Writing byte by byte
+            String str2 = "Writing each byte at a time\n";
+            byte b1[] = str2.getBytes();
+            for(byte x : b1)
+                fos.write(x);
+            
+            //3rd Method - Using offset
+            String str3 = "Copy only some part of string\n";
+            byte b2[] = str3.getBytes();
+            
+            fos.write(b2,6,str3.length()-6);
             fos.close();
         }
         
