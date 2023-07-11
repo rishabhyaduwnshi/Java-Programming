14606ecf0af64e4bcfd685b31172ae655e0d7d7b
Rishabh Yadav <1998rishabh31@gmail.com>
2023-07-11 21:56:13 +0530

    Create Server.java

---
+++ Network Programming/Server.java
@@ -0,0 +1,48 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class Main
+{
+    public static void server() throws Exception
+    {
+	System.out.println("Server started");
+	
+        //Create a server object using ServerSocket class 
+        ServerSocket ss = new ServerSocket(2000);
+        
+        //accept the connect from client
+        Socket stk = ss.accept();
+        
+        //Streams for getting input from client
+        BufferedReader br = new BufferedReader(new InputStreamReader(stk.getInputStream()));
+        
+        //PrintStream for sending data back to client
+        PrintStream ps = new PrintStream(stk.getOutputStream());
+        
+        String msg;
+        StringBuilder sb;
+        
+        do
+        {
+            //reads the line socket sent
+            msg = br.readLine();
+            
+            //reverse the line
+            sb = new StringBuilder(msg);
+            sb.reverse();
+            msg = sb.toString();
+            
+            //for sending the data to client
+            ps.println(msg);
+        }
+        while(!msg.equals("dne"));
+    }
+    
+    public static void main(String args[]) throws Exception
+    {
+        //calling server
+        server();
+    }
+}
