8726ce8a13df8c069b7646fcf209ab66d2f6bb9a
Rishabh Yadav <1998rishabh31@gmail.com>
2023-07-16 18:31:47 +0530

    Create AddServlet.java

---
+++ Servlets/Calling Servlet from Servlet/AddServlet.java
@@ -0,0 +1,35 @@
+package com.rishabh;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+public class AddServlet extends HttpServlet
+{
+	
+		public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+		{
+			int firstnum = Integer.parseInt(request.getParameter("num1"));
+			int secondnum = Integer.parseInt(request.getParameter("num2"));
+			
+			int sum = firstnum+secondnum;
+			
+			PrintWriter out = response.getWriter();
+			out.println("Sum of the numbers is : "+sum);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("square");
+			rd.forward(request, response);
+			
+			
+			
+		}
+		
+		public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+		{
+			doPost(request,response);
+		}
+}
